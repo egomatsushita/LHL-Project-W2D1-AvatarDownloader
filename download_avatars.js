5060fe606ec99a4b10edf081bcbf9d47b014bd30
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-03 22:25:36 +0000

    Implement downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var fs = require('fs');
 
 var GITHUB_USER = "egomatsushita";
 var GITHUB_TOKEN = "568b7c134d4568ef1761d64d7a9d4242944630f6";
@@ -22,13 +23,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
     if(response.statusCode === 200) {
       var json = JSON.parse(body);
       for(var avatar of json) {
-        cb(err, avatar.avatar_url);
+        cb(err, avatar);
       }
     }
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .on('error', function(err) {
+          throw err;
+         })
+         .on('response', function(response) {
+
+         })
+         .pipe(fs.createWriteStream(filePath));
+}
+
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-})
+  // console.log("Errors:", err);
+  // console.log("Result:", result.avatar_url);
+  downloadImageByURL(result.avatar_url, `./avatars/${result.login}.jpg`);
+
+});
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
