a524565a36d8f97b70561513ff040497ec74394d
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-03 21:54:07 +0000

    Print all the avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -6,9 +6,7 @@ var GITHUB_TOKEN = "568b7c134d4568ef1761d64d7a9d4242944630f6";
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // var githubURL = `https://github.com/${repoOwner}/${repoName}`;
   var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  console.log(requestURL);
 
   var requestOptions = {
     url: requestURL,
@@ -21,26 +19,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
     if(err) {
       throw err;
     }
-    console.log("inside request", response.statusCode);
     if(response.statusCode === 200) {
-      console.log("success");
       var json = JSON.parse(body);
-      console.log(json);
-    }
-
-  });
-
-  // console.log(requestURL);
-
-}
-    /*if(response.statusCode === 200) {
-      var json = JSON.parse(body);
-      // callback(json);
-      for(var i = 0; i < r; i++) {
-        var beer = json.data[i];
-        console.log(beer.name);
+      for(var avatar of json) {
+        cb(err, avatar.avatar_url);
       }
-    }*/
+    }
+  });
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
