de60591a2be9e58f781f1f7745107fe977a15671
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-03 21:40:04 +0000

    Make the request and print body

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,46 @@
 var request = require("request");
+
 var GITHUB_USER = "egomatsushita";
 var GITHUB_TOKEN = "568b7c134d4568ef1761d64d7a9d4242944630f6";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var githubURL = `https://github.com/${repoOwner}/${repoName}`;
+  // var githubURL = `https://github.com/${repoOwner}/${repoName}`;
   var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-
-  request.get(githubURL);
-
   console.log(requestURL);
 
+  var requestOptions = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'egomatsushita'
+    }
+  }
+
+  request(requestOptions, function(err, response, body) {
+    if(err) {
+      throw err;
+    }
+    console.log("inside request", response.statusCode);
+    if(response.statusCode === 200) {
+      console.log("success");
+      var json = JSON.parse(body);
+      console.log(json);
+    }
+
+  });
+
+  // console.log(requestURL);
+
 }
+    /*if(response.statusCode === 200) {
+      var json = JSON.parse(body);
+      // callback(json);
+      for(var i = 0; i < r; i++) {
+        var beer = json.data[i];
+        console.log(beer.name);
+      }
+    }*/
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
