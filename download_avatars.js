56fb11bc8dc80b70ad3baf9af6ca28636bb4cc58
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-03 19:41:58 +0000

    Create requestURL variable

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,16 @@
 var request = require("request");
+var GITHUB_USER = "egomatsushita";
+var GITHUB_TOKEN = "568b7c134d4568ef1761d64d7a9d4242944630f6";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var githubURL = `https://github.com/${repoOwner}/${repoName}`;
+  var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
 
   request.get(githubURL);
-  console.log("hey");
+
+  console.log(requestURL);
 
 }
 
