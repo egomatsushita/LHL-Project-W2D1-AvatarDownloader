d6935273e9217841bc7e6d39d6c7fdd75c630f18
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-03 19:21:59 +0000

    Add getRepoContributors function with 3 parameters

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,16 @@
 var request = require("request");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var githubURL = `https://github.com/${repoOwner}/${repoName}`;
+
+  request.get(githubURL);
+  console.log("hey");
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+})
