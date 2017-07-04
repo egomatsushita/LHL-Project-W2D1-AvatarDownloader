b0bc72963b6b2c2bf1021ccf5e9d047829a82033
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-04 00:47:40 +0000

    Add some comments

--- download_avatars.js
+++ download_avatars.js
@@ -5,16 +5,17 @@ var arg = process.argv.slice(2);
 var repoOwner = arg[0];
 var repoName = arg[1];
 
+// Variables that get GitHub user and token from a hidden file
 var GITHUB_USER = process.env.DB_USER;
 var GITHUB_TOKEN = process.env.DB_TOKEN;
 
+// Checking if there are two command line arguments
 if(arg.length !== 2) {
   console.log("*** Please input repo-owner and repo-name ***\nAnd follow this pattern: node <file> <owner> <repo>");
   return;
 }
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-
+// Function that request for a GitHub content and retrieves users data
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
 
@@ -38,6 +39,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+// Function that download GitHub avatar image by passing an url and a path as parameters
 function downloadImageByURL(url, filePath) {
   request.get(url)
          .on('error', function(err) {
@@ -49,6 +51,7 @@ function downloadImageByURL(url, filePath) {
          .pipe(fs.createWriteStream(filePath));
 }
 
+// Calling getRepoContributors function
 getRepoContributors(repoOwner, repoName, function(err, result) {
   downloadImageByURL(result.avatar_url, `./avatars/${result.login}.jpg`);
 });
